ce1763db2d2e2730426abb9c0626aee5cc12f741
Allan <2231513@aluno.univesp.br>
2024-05-04 12:05:24 -0300

    background tela login

--- GarageHub/garage/urls.py
+++ GarageHub/garage/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path, include
+
 from .views import (
     ClienteListView,
     ClienteDetailView,
@@ -21,24 +22,24 @@ from .views import (
 app_name = 'garage'
 
 urlpatterns = [
-    path('', HomeListView.as_view(), name='garage-home'),
-    path('clientes/', ClienteListView.as_view(), name='client-view'),
-    path('clientes/<int:pk>/', ClienteDetailView.as_view(), name='client-detail'),
+    path('', HomeListView.as_view(), name='garage-home'),  # Corrigido o nome da URL
+    path('clientes/', ClienteListView.as_view(), name='client-list'),
     path('clientes/new/', ClienteCreateView.as_view(), name='client-create'),
+    path('clientes/<int:pk>/', ClienteDetailView.as_view(), name='client-detail'),
     path('clientes/<int:pk>/update/', ClienteUpdateView.as_view(), name='client-update'),
     path('clientes/<int:pk>/delete/', ClienteDeleteView.as_view(), name='client-delete'),
     
-    path('veiculos/', VeiculoListView.as_view(), name='veiculo-view'),
-    path('veiculos/<int:pk>/', VeiculoDetailView.as_view(), name='veiculo-detail'),
+    path('veiculos/', VeiculoListView.as_view(), name='veiculo-list'),
     path('veiculos/new/', VeiculoCreateView.as_view(), name='veiculo-create'),
+    path('veiculos/<int:pk>/', VeiculoDetailView.as_view(), name='veiculo-detail'),
     path('veiculos/<int:pk>/update/', VeiculoUpdateView.as_view(), name='veiculo-update'),
     path('veiculos/<int:pk>/delete/', VeiculoDeleteView.as_view(), name='veiculo-delete'),
     
-    path('ordens/', OrdemListView.as_view(), name='ordem-view'),
-    path('ordens/<int:pk>/', OrdemDetailView.as_view(), name='ordem-detail'),
+    path('ordens/', OrdemListView.as_view(), name='ordem-list'),
     path('ordens/new/', OrdemCreateView.as_view(), name='ordem-create'),
+    path('ordens/<int:pk>/', OrdemDetailView.as_view(), name='ordem-detail'),
     path('ordens/<int:pk>/update/', OrdemUpdateView.as_view(), name='ordem-update'),
     path('ordens/<int:pk>/delete/', OrdemDeleteView.as_view(), name='ordem-delete'),
-
+    
     path('admin/', include('grappelli.urls')),
 ]
--- GarageHub/garage/views.py
+++ GarageHub/garage/views.py
@@ -1,46 +1,14 @@
-# from django.forms import BaseModelForm
-# from django.http import HttpResponse, HttpResponseRedirect
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render
 from django.views.generic import (
     ListView,
     DetailView,
     CreateView,
     UpdateView,
     DeleteView
-    )
+)
+from django.urls import reverse_lazy
 from .models import Cliente, Veiculo, Ordem
 from .forms import ClientesForm, VeiculosForm, OrdemForm
-
-# from django.contrib.auth.mixins import LoginRequiredMixin
-
-# def get_name(request):
-#     print('esta entrando aqui')
-#     if request.method == 'POST':
-#         form = VeiculosForm(request.POST)
-#         if form.is_valid():
-#             print('esta entrando aqui1')
-            
-#             return HttpResponseRedirect('/')
-#     else:
-#         # form = OrdemForm()
-#         form = VeiculosForm()
-#         print('esta entrando aqui2')
-#     return render(request, 'garage/ordens/name.html', {'form':form})
-
-# def post_list(request):
-#     posts = Post.published.all()
-#     return render(request, 'garage/post/list.html', {'posts':posts})
-
-# def post_detail(request, id):
-#     post = get_object_or_404(Post, id=id, status=Post.Status.PUBLISHED)
-    
-#     return render(request, 'garage/post/detail.html', {'post': post})
-
-def home(request):
-    context = {
-        'clientes': Ordem.objects.all()
-    }
-    return render(request, 'garage/home.html', context)
 
 
 class HomeListView(ListView):
@@ -55,65 +23,76 @@ class ClienteListView(ListView):
     template_name = 'garage/clientes/client_home.html'
     context_object_name = 'clientes'
     ordering = ['-data_criacao']
-    
+
+
 class ClienteDetailView(DetailView):
     model = Cliente
     template_name = 'garage/clientes/client_details.html'
-    
+
+
 class ClienteCreateView(CreateView):
     model = Cliente
     template_name = 'garage/clientes/client_create_form.html'
     form_class = ClientesForm
-    
- 
+    success_url = reverse_lazy('garage:client-list')
+
+
 class ClienteDeleteView(DeleteView):
     model = Cliente
-    success_url = '/garage/clientes'
+    success_url = reverse_lazy('garage:client-list')
     template_name = 'garage/clientes/client_confirm_delete.html'
-    
+
+
 class ClienteUpdateView(UpdateView):
     model = Cliente
     fields = ['nome', 'n_cpf', 'endereco', 'bairro', 'cidade', 'cep']
     template_name = 'garage/clientes/client_update_form.html'
+    success_url = reverse_lazy('garage:client-list')
 
-    
+
 class VeiculoListView(ListView):
     model = Veiculo
     template_name = 'garage/veiculos/veiculo_home.html'
     context_object_name = 'veiculos'
     ordering = ['-data_criacao']
-    
+
+
 class VeiculoDetailView(DetailView):
     model = Veiculo
     template_name = 'garage/veiculos/veiculo_details.html'
-    
+
+
 class VeiculoCreateView(CreateView):
     model = Veiculo
     template_name = 'garage/veiculos/veiculo_create_form.html'
     form_class = VeiculosForm
-    
-    
+    success_url = reverse_lazy('garage:veiculo-list')
+
+
 class VeiculoDeleteView(DeleteView):
     model = Veiculo
-    success_url = '/garage/veiculos'
+    success_url = reverse_lazy('garage:veiculo-list')
     template_name = 'garage/veiculos/veiculo_confirm_delete.html'
-    
+
+
 class VeiculoUpdateView(UpdateView):
     model = Veiculo
     fields = ['marca','modelo', 'placa', 'motor', 'ano']
     template_name = 'garage/veiculos/veiculo_update_form.html'
+    success_url = reverse_lazy('garage:veiculo-list')
     
     def form_valid(self, form):
         form.instance.author = self.request.user
         return super().form_valid(form)
-    
+
 
 class OrdemListView(ListView):
     model = Ordem
     template_name = 'garage/ordens/ordem_home.html'
     context_object_name = 'ordens'
     ordering = ['-cliente_id']
-    
+
+
 class OrdemDetailView(DetailView):
     model = Ordem
     template_name = 'garage/ordens/ordem_details.html'
@@ -123,19 +102,28 @@ class OrdemCreateView(CreateView):
     model = Ordem
     template_name = 'garage/ordens/ordem_create_form.html'
     form_class = OrdemForm
+    success_url = reverse_lazy('garage:ordem-list')
 
-    
+
 class OrdemDeleteView(DeleteView):
     model = Ordem
-    success_url = '/garage/ordens'
+    success_url = reverse_lazy('garage:ordem-list')
     template_name = 'garage/ordens/ordem_confirm_delete.html'
-    
+
+
 class OrdemUpdateView(UpdateView):
     model = Ordem
     form_class = OrdemForm
     template_name = 'garage/ordens/ordem_update_form.html'
-    success_url = '/garage/ordens'
+    success_url = reverse_lazy('garage:ordem-list')
     
     def form_valid(self, form):
         form.instance.author = self.request.user
         return super().form_valid(form)
+
+
+def home(request):
+    context = {
+        'ordens': Ordem.objects.all()
+    }
+    return render(request, 'garage/home.html', context)
